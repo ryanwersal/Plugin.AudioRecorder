31da4a9e95b3036b2ce02516341e5c085ef49389
Ryan Wersal <ryan@ryanwersal.com>
2018-05-01 16:35:26 -0500

    Allow specifying custom FilePath for recording
    If FilePath is not specified, the original behavior will be the
    default.
    
    This change does alter the overridding behavior for properties such
    as PreferredSampleRate on Android as it previously would allow
    the user's value to override what the Android platform was. The new
    behavior would result in the Android platform value being evaluated
    after the user's value had been set (rather than before).

--- Plugin.AudioRecorder.UWP/AudioRecorderService.cs
+++ Plugin.AudioRecorder.UWP/AudioRecorderService.cs
@@ -1,16 +1,19 @@
-﻿using System;
+using System;
 using Windows.Storage;
 
 namespace Plugin.AudioRecorder
 {
-    public partial class AudioRecorderService
-    {
-        async partial void Init()
-        {
-            StorageFolder temporaryFolder = ApplicationData.Current.TemporaryFolder;
-            StorageFile tempFile = await temporaryFolder.CreateFileAsync(RecordingFileName, CreationCollisionOption.ReplaceExisting);
+	public partial class AudioRecorderService
+	{
+		async partial void Init()
+		{
+			if (FilePath == null)
+			{
+				StorageFolder temporaryFolder = ApplicationData.Current.TemporaryFolder;
+				StorageFile tempFile = await temporaryFolder.CreateFileAsync(DefaultFileName, CreationCollisionOption.ReplaceExisting);
 
-            filePath = tempFile.Path;
-        }
-    }
+				FilePath = tempFile.Path;
+			}
+		}
+	}
 }
